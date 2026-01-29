90ad2ab317e56ff740544b69062c70261282f422
Harsimran Sidhu <22066581+PKHarsimran@users.noreply.github.com>
2026-01-29 12:03:59 -0400

    Add discovery functions for MQTT bridge

--- ewelink_bridge/app/discovery.js
+++ ewelink_bridge/app/discovery.js
@@ -1,1 +1,23 @@
+function discoveryTopic(discoveryPrefix, component, objectId) {
+  return `${discoveryPrefix}/${component}/${objectId}/config`;
+}
 
+function switchDiscoveryPayload({ name, uniqueId, stateTopic, commandTopic, availabilityTopic }) {
+  return {
+    name,
+    uniq_id: uniqueId,
+    stat_t: stateTopic,
+    cmd_t: commandTopic,
+    avty_t: availabilityTopic,
+    pl_on: "ON",
+    pl_off: "OFF",
+    dev: {
+      ids: [uniqueId],
+      name: "eWeLink MQTT Bridge",
+      mf: "Community",
+      mdl: "Cloud-first"
+    }
+  };
+}
+
+module.exports = { discoveryTopic, switchDiscoveryPayload };
