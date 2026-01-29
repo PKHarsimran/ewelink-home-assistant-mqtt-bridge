af4a9d651fcb14c967a7974fbf5bcb5fb87a4367
Harsimran Sidhu <22066581+PKHarsimran@users.noreply.github.com>
2026-01-29 12:03:48 -0400

    Add MQTT connection function with logging

--- ewelink_bridge/app/mqtt.js
+++ ewelink_bridge/app/mqtt.js
@@ -1,1 +1,19 @@
+const mqtt = require("mqtt");
 
+function connectMqtt({ host, port, username, password, log }) {
+  const url = `mqtt://${host}:${port}`;
+  const opts = {};
+
+  if (username) opts.username = username;
+  if (password) opts.password = password;
+
+  const client = mqtt.connect(url, opts);
+
+  client.on("connect", () => log("info", `MQTT connected: ${url}`));
+  client.on("reconnect", () => log("warn", "MQTT reconnecting..."));
+  client.on("error", (err) => log("error", `MQTT error: ${err.message}`));
+
+  return client;
+}
+
+module.exports = { connectMqtt };
