3c7d31fb11144c5685eca563d640d08ca95013de
Harsimran Sidhu <22066581+PKHarsimran@users.noreply.github.com>
2026-01-29 12:07:27 -0400

    Delete ewelink_bridge directory

--- ewelink_bridge/app/devices.simulator.js
+++
@@ -1,8 +0,0 @@
-function getSimulatedDevices() {
-  return [
-    { id: "sim_1", name: "Sim Lamp", type: "switch", state: "OFF" },
-    { id: "sim_2", name: "Sim Fan", type: "switch", state: "ON" }
-  ];
-}
-
-module.exports = { getSimulatedDevices };
